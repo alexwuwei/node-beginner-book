2499d8dabecab262a7a1068e714e8d483292c4d4
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-02 11:00:22 -0800

    added pathname parsing to onRequest function

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var http = require('http');
 
 function start() {
 function onRequest(request, response) {
+  var pathname = url.parse(request.url).pathname;
+  console.log('A request for ' + pathname + 'was received.');
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hello World');
   response.end();
 }
 
 http.createServer(onRequest).listen(8888);
-}
-
 console.log('Server has started.');
+}
 
 exports.start = start;
